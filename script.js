ddd3368e79bdd7373729cfb6cc14b46bfb6b553a
Roxana Daraban <roxana.daraban@student.upt.ro>
2025-09-15 18:05:43 +0300

    guess too low

--- script.js
+++ script.js
@@ -36,6 +36,17 @@ if(!guess){
     document.querySelector('.message').textContent='You lost the game';
     document.querySelector('.score').textContent=0;
   }
+
+//when the guess is too low
+}else if(guess<secretNumber){
+  if(score>1){
+    document.querySelector('.message').textContent='Too low!';
+    score--;
+    document.querySelector('.score').textContent=score;
+  }else{
+    document.querySelector('.message').textContent='You lost the game';
+    document.querySelector('.score').textContent=0;
+  }
 }
 })
 
