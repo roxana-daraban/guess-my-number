2a59790e6ecb8d3bed066d0eec054f67590568df
Roxana Daraban <roxana.daraban@student.upt.ro>
2025-09-15 18:17:40 +0300

    button again

--- script.js
+++ script.js
@@ -51,3 +51,17 @@ if(!guess){
 })
 
 
+document.querySelector('.again').addEventListener('click',function(){
+  score=20;
+  secretNumber=Math.trunc(Math.random()*20+1);
+  document.querySelector('.score').textContent=score;
+  document.querySelector('.number').textContent='?';
+  document.querySelector('.message').textContent='Start guessing...';
+  document.querySelector('.guess').value='';
+  
+  document.querySelector('body').style.backgroundColor='#222';
+  document.querySelector('.number').style.width='15rem';
+
+})
+
+
